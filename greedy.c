1d34f05a5c37fd0caac96419ae9b241d5f30b073
Tron8D <dima8b94@gmail.com>
2016-09-14 22:44:52 +0300

    2

---
+++ greedy.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+
+{
+    float OddMoney = 0;
+    
+    do {
+        printf("How much change is owed?\n");
+        OddMoney = GetFloat();
+    } while (OddMoney < 0);
+   
+    int Coins[] = {25 , 10 , 5 , 1};
+    int MinCoins = 0;
+    int Cent = OddMoney*100 + 0.5;
+    int n = 0;
+    
+        do {
+            if (Cent>=Coins[n]){
+                Cent-=Coins[n];
+                MinCoins++;
+            } 
+            else {
+                n++;
+            }
+        }
+        while ( Cent > 0 );
+        
+        printf("%d\n", MinCoins);
+    return 0;
+}
